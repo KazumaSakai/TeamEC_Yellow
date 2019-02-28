e246592e86e9646e8d9eac01b79176f8d4192670
kazuma_sakai <39910750+KazumaSakai@users.noreply.github.com>
2019-02-28 13:36:52 +0900

    Create MCategoryDTO.java

---
+++ MCategoryDTO.java
@@ -0,0 +1,56 @@
+package com.internousdev.yellow.dto;
+
+import java.sql.Date;
+
+/**
+ * 商品カテゴリ情報
+ *
+ * @author 酒井和馬
+ *
+ */
+public class MCategoryDTO
+{
+	private int id;
+	private int categoryId;
+	private String categoryName;
+	private String categoryDescription;
+	private Date insertDate;
+	private Date updateDate;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getCategoryDescription() {
+		return categoryDescription;
+	}
+	public void setCategoryDescription(String categoryDescription) {
+		this.categoryDescription = categoryDescription;
+	}
+	public Date getInsertDate() {
+		return insertDate;
+	}
+	public void setInsertDate(Date insertDate) {
+		this.insertDate = insertDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+}
