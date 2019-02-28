fec2a574e0895d07b9a303ace90f0a469c911ac7
kazuma_sakai <39910750+KazumaSakai@users.noreply.github.com>
2019-02-28 13:35:05 +0900

    Create HomeAction.java

---
+++ HomeAction.java
@@ -0,0 +1,42 @@
+package com.internousdev.yellow.action;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.yellow.dao.MCategoryDAO;
+import com.internousdev.yellow.dto.MCategoryDTO;
+import com.internousdev.yellow.util.CommonUtility;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport implements SessionAware
+{
+	//	Session
+	private Map<String, Object> session;
+
+	public String execute()
+	{
+		//	商品カテゴリを取得
+		MCategoryDAO mcategoryDAO = new MCategoryDAO();
+		List<MCategoryDTO > mCategoryDtoList = mcategoryDAO.getMCategoryList();
+		session.put("mCategoryDtoList", mCategoryDtoList);
+
+		//未ログイン時のときにtempUserIdの発行
+		if(!(session.containsKey("loginId")) && !(session.containsKey("tempUserId")))
+		{
+			CommonUtility commonUtility = new CommonUtility();
+			session.put("tempUserId", commonUtility.getRamdomValue());
+		}
+
+		return SUCCESS;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+}
